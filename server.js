04e2b47d69118ab7869f1e5cf3c781a6cdd9dc5b
Saurabh <saurabhnitnaware@gmail.com>
2016-10-06 14:52:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,8 +78,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName',function(req,res){
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    var aName = req.params.articleName;
+    res.send(createTemplate(articles[aName]));
 });
 
 
