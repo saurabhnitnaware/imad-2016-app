30b044dfe859e13177637a0f8323ae67b53ec5e8
Saurabh <saurabhnitnaware@gmail.com>
2016-10-06 15:49:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,10 +5,10 @@
 var bro = document.getElementById('bro');
 var marginLeft = 0;
 function moveRight(){
-    marginLeft += 10;
+    marginLeft += 5;
     bro.style.marginLeft = marginLeft + 'px';
 }
 
 bro.onclick = function(){
-  var interval = setInterval(moveRight,1000);
+  var interval = setInterval(moveRight,70);
 };
