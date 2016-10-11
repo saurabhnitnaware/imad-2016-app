8abfbc44b5385a805850e3e4da09d9f4c4a92b4e
Saurabh <saurabhnitnaware@gmail.com>
2016-10-11 17:44:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,23 +13,23 @@ bro.onclick = function(){
   var interval = setInterval(moveRight,70);
 };
 // incrementing counter by button
-var counter = 0;
-var button = document.getElementById("but");
-button.onclick = function(){
-  
-  var request = new XMLHttpRequest();
-  
-  request.onreadystatechange = function(){
-    if(request.readyState == XMLHttpRequest.DONE)
-    {
-        if(request.status == 200)
+    var counter = 0;
+    var button = document.getElementById("but");
+    button.onclick = function(){
+      
+      var request = new XMLHttpRequest();
+      
+      request.onreadystatechange = function(){
+        if(request.readyState == XMLHttpRequest.DONE)
         {
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
+            if(request.status == 200)
+            {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
         }
-    }
-  };
+    };
   
   /*counter = counter + 1;
   var span = document.getElementById("count");
