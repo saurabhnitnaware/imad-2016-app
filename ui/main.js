8275c6fb08953516d40ce2af948d7c0970261f76
Saurabh <saurabhnitnaware@gmail.com>
2016-10-12 13:50:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,19 +42,35 @@ button.onclick = function(){
 };
 
 // 
-var InputName = document.getElementById('name');
-var nameF = InputName.value;
+
 var but = document.getElementById('submitBtn');
 
 but.onclick = function(){
-  var names = ["Saurabh","Rahul","Sammer"];
-  var list = '';
-  for(var i=0;i<names.length;i++)
-  {
-      list += '<li> ' +names[i]+ ' </li>';
-  }
-  var ul = document.getElementById('uo');
-  ul.innerHTML = list;
+// making request to server    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE)
+      {
+          if(request.status === 200){
+              var names = request.responseText;
+              names = JSON.parse(names);
+              // print the name    
+              var list = '';
+              for(var i=0;i<names.length;i++)
+              {
+                  list += '<li> ' +names[i]+ ' </li>';
+              }
+              var ul = document.getElementById('uo');
+              ul.innerHTML = list;
+          }
+      }
+    };
+
+var InputName = document.getElementById('name');
+var nameF = InputName.value;
+ request.open('GET','http://saurabhnitnaware.imad.hasura-app.io/submit-name?name=' + nameF,true);
+ request.send(null);
   
 };
 
