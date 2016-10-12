f114f0088765e0513a7243b266f6138553af4461
Saurabh <saurabhnitnaware@gmail.com>
2016-10-12 13:15:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,12 +49,13 @@ var but = document.getElementById('submitBtn');
 but.onclick = function(){
   var names = ["Saurabh","Rahul","Sammer"];
   var list = '';
-  for(var i=0;i<names.length;i++)
+  for(var i=0;i<=names.length;i++)
   {
       list = '<li>'+names[i]+'</li>';
-  }
+
   var ul = document.getElementById('uo');
   ul.innerHTML = list;
+  }
 };
 
 
