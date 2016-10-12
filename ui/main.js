317bcbcfbd74bbab82ac260707cdc72cc685a79a
Saurabh <saurabhnitnaware@gmail.com>
2016-10-12 13:19:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -51,7 +51,7 @@ but.onclick = function(){
   var list = '';
   for(var i=0;i<names.length;i++)
   {
-      list += '<li>'+names[i]+'</li>';
+      list += '<li>' +names[i]+ '</li>';
   }
   var ul = document.getElementById('uo');
   ul.innerHTML = list;
